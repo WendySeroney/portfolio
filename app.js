a71450c5decf487b2f0ba7bff049d709cae1505a
WendySeroney <seroneywendy@gmail.com>
2026-04-16 12:33:34 +0300

    made changes to my code

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const mobile = document.querySelector('.header .nav-bar .nav-list ul');
+const menu = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+	mobile.classList.toggle('active');
 });
+
+
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
@@ -20,6 +22,6 @@ document.addEventListener('scroll', () => {
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
+		mobile.classList.toggle('active');
 	});
 });
